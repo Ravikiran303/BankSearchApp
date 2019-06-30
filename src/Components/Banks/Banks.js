b0a2506f381384e27e0e5c3edaf7859cb3281064
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-30 23:50:36 +0530

    Add banks component

---
+++ src/Components/Banks/Banks.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import axios from "axios";
+import "react-table/react-table.css";
+
+export class Banks extends Component {
+  render() {
+    return <ReactTable />;
+  }
+}
+
+export default Banks;
