2bfef63df538dfb37fb838b73d381c1046f8a853
Ravikiran303 <ravikiran2221@gmail.com>
2019-07-01 10:37:59 +0530

    Move api to app file and implement search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Banks from "./Components/Banks/Banks";
+import { SeachBar } from "./Components/SearchBar/SeachBar";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Banks />
-    </div>
-  );
+class App extends Component {
+  state = {
+    banks: []
+  };
+  componentDidMount() {
+    axios
+      .get("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI")
+      .then(response => {
+        const banks = response.data;
+        this.setState({ banks });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  searchKeywordChange = search => {
+    const filtered = this.state.banks.filter(bank => bank === search);
+    this.setState({ banks: filtered });
+    console.log(filtered);
+  };
+  render() {
+    return (
+      <div className="App">
+        <SeachBar searchKeywordChange={this.searchKeywordChange} />
+        <Banks />
+      </div>
+    );
+  }
 }
 
 export default App;
