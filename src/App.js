ad517e1fab79019f45169f68287e38dbc202d1dd
Ravikiran303 <ravikiran2221@gmail.com>
2019-07-02 09:26:11 +0530

    Add search and dropdown functionality

--- src/App.js
+++ src/App.js
@@ -2,32 +2,70 @@ import React, { Component } from "react";
 import Banks from "./Components/Banks/Banks";
 import { SeachBar } from "./Components/SearchBar/SeachBar";
 import axios from "axios";
+import Cities from "./Components/Cities/Cities";
+import "./App.css";
 
 class App extends Component {
   state = {
-    banks: []
+    banks: [],
+    filteredBanks: [],
+    loading: true
   };
-  componentDidMount() {
+
+  async componentDidMount() {
     axios
       .get("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI")
       .then(response => {
         const banks = response.data;
-        this.setState({ banks });
+        this.setState({ banks: banks, filteredBanks: banks, loading: false });
       })
+
       .catch(err => {
         console.log(err);
       });
   }
-  searchKeywordChange = search => {
-    const filtered = this.state.banks.filter(bank => bank === search);
-    this.setState({ banks: filtered });
-    console.log(filtered);
+  filterCities = value => {
+    const filtered = this.state.banks.filter(function(bank, index, arr) {
+      let { city = "" } = bank;
+      return city.toLowerCase() === value.value.toLowerCase();
+    });
+    this.setState({ filtered_arr: filtered });
   };
+  searchKeywordChange = search => {
+    const filtered = this.state.banks.filter(function(bank, index, arr) {
+      let {
+        bank_name = "",
+        state = "",
+        district = "",
+        city = "",
+        address = "",
+        branch = "",
+        bank_id = "",
+        ifsc = ""
+      } = bank;
+
+      return (
+        bank_name.toLowerCase().includes(search.toLowerCase()) ||
+        state.toLowerCase().includes(search.toLowerCase()) ||
+        district.toLowerCase().includes(search.toLowerCase()) ||
+        city.toLowerCase().includes(search.toLowerCase()) ||
+        address.toLowerCase().includes(search.toLowerCase()) ||
+        branch.toLowerCase().includes(search.toLowerCase()) ||
+        ifsc.toLowerCase().includes(search.toLowerCase()) ||
+        bank_id === parseInt(search)
+      );
+    });
+    this.setState({ filteredBanks: filtered });
+  };
+
   render() {
     return (
       <div className="App">
-        <SeachBar searchKeywordChange={this.searchKeywordChange} />
-        <Banks />
+        <div className="filter">
+          <Cities filterCities={this.filterCities} />
+          <SeachBar searchKeywordChange={this.searchKeywordChange} />
+        </div>
+        <Banks banks={this.state.filteredBanks} loading={this.state.loading} />
       </div>
     );
   }
